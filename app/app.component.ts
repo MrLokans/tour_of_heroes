33ad12fe961d1d950506976cb6647ee1ba68a12f
MrLokans <trikster1911@gmail.com>
2016-04-29 07:51:22 +0300

    Hello world completed. Use 'npm start' to run it.

---
+++ app/app.component.ts
@@ -0,0 +1,10 @@
+import {Component} from 'angular2/core';
+
+
+@Component({
+    selector:'my-app',
+    template: '<h1>Hello World!</h1>'
+    // Angular creates and displays an instance of our AppComponent wherever it encounters a my-app element in the host HTML.
+})
+
+export class AppComponent { }
