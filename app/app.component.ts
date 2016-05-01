67e8ff7afa1c391a60833d97e422d9d10101ecd3
MrLokans <trikster1911@gmail.com>
2016-05-01 20:42:53 +0300

    Hero class added, introducing two-way data-binding and new directive syntax.

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,9 +2,29 @@ import {Component} from 'angular2/core';
 
 
 @Component({
+
     selector:'my-app',
-    template: '<h1>Hello World!</h1>'
+    template: `
+    <h1>{{title}}</h1>
+    <h2>{{hero.name}} details!</h2>
+    <div><label>id: </label>{{ hero.id }}</div>
+    <div>
+        <label>name: </label>{{ hero.name }}
+        <input [(ngModel)]="hero.name" placeholder="name"/>
+    </div>
+    `
     // Angular creates and displays an instance of our AppComponent wherever it encounters a my-app element in the host HTML.
 })
 
-export class AppComponent { }
+export class AppComponent { 
+    title='Tour of heroes';
+    hero: Hero = {
+        id: 1,
+        name: 'Windstorm'
+    };
+}
+
+export class Hero {
+    id: number;
+    name: string;
+}
